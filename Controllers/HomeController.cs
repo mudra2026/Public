f897adcd1aacc8d39dc14d45b674e48a26d6d85c
mudra2026 <kamblemudra34@gmail.com>
2026-08-24 23:43:21 +0530

    Add WhatsApp enquiry fallback

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -67,6 +67,8 @@ public class HomeController : Controller
         await _emailService.NotifyOwnerAsync(enquiry);
 
         TempData["EnquirySubmitted"] = "Thank you. We will contact you shortly.";
+        var whatsappMessage = $"New enquiry from {enquiry.Name}\nPhone: {enquiry.Phone}\nEmail: {enquiry.Email}\nService: {enquiry.Service}\nDetails: {enquiry.Message}";
+        TempData["WhatsAppLink"] = $"https://wa.me/919987802727?text={Uri.EscapeDataString(whatsappMessage)}";
         return RedirectToAction(nameof(Contact));
     }
 
